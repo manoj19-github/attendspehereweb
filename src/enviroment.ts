400ea99252804de87433c4c7612cb225bcc92660
manojs-sudo <manojs@msqube.com>
2026-05-06 01:44:36 +0530

    fixed

--- src/enviroment.ts
+++ src/enviroment.ts
@@ -1,2 +1,6 @@
- export const backendUrl="http://localhost:5000/api";
-export const socketServerURL="http://localhost:5000/";
+//  export const backendUrl="http://localhost:5000/api";
+// export const socketServerURL="http://localhost:5000/";
+ export const backendUrl=`${window.location.protocol}//${window.location.host}/api/`
+export const socketServerURL=`${window.location.protocol}//${window.location.host}/`;
+
+ //export const baseServiceUrl = `${window.location.protocol}//${window.location.host}/api/`;
